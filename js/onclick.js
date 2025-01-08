dee174bfa06e9330853770e654d81d8bd3b1e6a0
Aditya Chauhan <chauhan.aditya7051@gmail.com>
2025-01-08 14:31:38 +0530

    removed_reduntant

--- js/onclick.js
+++ js/onclick.js
@@ -8,34 +8,6 @@ function hideMenu() {
     navLinks.style.right = "-200px";
 }
 
-// For desktop, ensure `navLinks` is not set to an incorrect position.
-navLinks.style.right = "0"; // Add this to ensure it’s visible on desktop.
 
-
-// Popup JS
-
-
-// const popupScreen = document.querySelector(".popup-screen");
-// const popuoBox = document.querySelector(".popup-box");
-// const closeBtn = document.querySelector(".close-btn");
-
-// window.addEventListener("load", () => {
-//     setTimeout(() => {
-//         popupScreen.classList.add("active");
-//     }, 2000);
-// });
-
-// closeBtn.addEventListener("click", () => {
-//     popupScreen.classList.remove("active");
-
-//     document.cookie = "WebsiteName = ANARC; max-age=" + 24*60*60;
-// });
-
-// const WebsiteCookie = document.cookie.indexOf("WebsiteName=");
-
-// if (WebsiteCookie != -1) {
-//     popupScreen.style.display = "none";
-// } else {
-//     popupScreen.style.display = "flex";
-// }
+navLinks.style.right = "0"; 
 
